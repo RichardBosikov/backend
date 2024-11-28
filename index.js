424479905a5583e12fccc2622e652d3f04fb91a4
Richard <daniil_bosikov@mail.ru>
2024-11-28 16:46:55 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// const { require } = require("yargs");
+const yargs = require("yargs");
+
+const { addNote, printNotes, removeNote } = require("./notes.controller");
+const { command } = require("yargs");
+
+yargs.command({
+  command: "add",
+  describe: "Add new note to list",
+  builder: {
+    title: {
+      type: "string",
+      describe: "Note title",
+      demandOption: true,
+    },
+  },
+  handler({ title }) {
+    addNote(title);
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "Print all notes",
+  async handler() {
+    printNotes();
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove note",
+  builder: {
+    id: {
+      type: "string",
+      describe: "Note id",
+      demandOption: true,
+    },
+  },
+  async handler({ id }) {
+    removeNote(id);
+  },
+});
+
+yargs.parse();
